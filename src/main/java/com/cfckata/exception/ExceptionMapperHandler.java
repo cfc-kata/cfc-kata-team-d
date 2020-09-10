5cc7b99ef71b40cdf73f9340ebcf33123f01a6f1
Mei Xuesong <meixuesong@gmail.com>
2020-09-10 17:12:50 +0800

    add exception handler

---
+++ src/main/java/com/cfckata/exception/ExceptionMapperHandler.java
@@ -0,0 +1,29 @@
+package com.cfckata.exception;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.persistence.EntityNotFoundException;
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class ExceptionMapperHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> exceptionHandler(Exception exception) {
+        logger.error("ResponseException exception: {}", exception);
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler({EntityNotFoundException.class})
+    public ResponseEntity<String> handleSizeExceededException(HttpServletRequest request, EntityNotFoundException exception) {
+        logger.error("EntityNotFoundException exception: {}", exception);
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+}
