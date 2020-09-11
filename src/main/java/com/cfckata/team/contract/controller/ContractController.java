6b8154fcd4cc91e778c706a44fafc8ee570deebb
wen_chao <wen_chao@suixingpay.com>
2020-09-11 11:00:48 +0800

    合同模块创建

---
+++ src/main/java/com/cfckata/team/contract/controller/ContractController.java
@@ -0,0 +1,18 @@
+package com.cfckata.team.contract.controller;
+
+import com.cfckata.team.contract.service.ContractService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/contract")
+public class ContractController {
+
+    private ContractService contractService;
+
+    public ContractController(ContractService contractService) {
+        this.contractService = contractService;
+    }
+
+
+}
---
+++ src/main/java/com/cfckata/team/contract/service/ContractService.java
@@ -0,0 +1,8 @@
+package com.cfckata.team.contract.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ContractService {
+
+}
